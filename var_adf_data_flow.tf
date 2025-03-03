7bcdc0d36d9f350050c130d4eed364cd26e694d0
proman <proman@gmail.com>
2025-03-03 12:33:53 +0530

    adding adf_data_flow code

--- var_adf_data_flow.tf
+++ var_adf_data_flow.tf
@@ -1,31 +1,37 @@
 variable "resource_group_name" {
   type        = string
   description = "The name of the resource group"
+  default = "rg"
 }
 
 variable "location" {
   type        = string
   description = "Azure region for the resources"
+  default = "uk"
 }
 
 variable "storage_account_name" {
   type        = string
   description = "The name of the Azure Storage Account"
+  default = "st"
 }
 
 variable "linked_service_name" {
   type        = string
   description = "The name of the linked service for Data Factory"
+  default = "null"
 }
 
 variable "data_flow_name" {
   type        = string
   description = "The name of the Data Flow"
+  default = "null"
 }
 
 variable "data_factory_id" {
   type        = string
   description = "The ID of the Data Factory"
+  default = "null"
 }
 
 variable "description" {
@@ -41,6 +47,12 @@ variable "sources" {
     schema              = string
   }))
   description = "List of source configurations"
+  default = [ {
+    name = "s"
+    dataset_name = "n"
+    schema = "sc"
+     type = "t"
+  } ]
 }
 
 variable "sinks" {
@@ -51,6 +63,12 @@ variable "sinks" {
     schema              = string
   }))
   description = "List of sink configurations"
+  default = [ {
+    dataset_name = "da"
+    name = "n"
+    schema = "sch"
+    type = "ty"
+  } ]
 }
 
 variable "transformations" {
@@ -60,4 +78,9 @@ variable "transformations" {
     description = string
   }))
   description = "List of transformations for the data flow"
+  default = [ {
+    description = "d"
+    name = "na"
+    type = "tyy"
+  } ]
 }
