6e138a945a193fe5e5bbdea9c47807e153e8792a
proman <proman@gmail.com>
2025-03-03 20:44:17 +0530

    adding keyvault link

--- key_link.tf
+++ key_link.tf
@@ -3,6 +3,5 @@ resource "azurerm_data_factory_linked_service_key_vault" "example" {
   data_factory_id = var.data_factory_id
   key_vault_id    = var.key_vault_id
   description     = "Linked Service connecting Data Factory to Azure Key Vault"
-
   }
 
