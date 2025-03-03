5dfa39ba686e860b6a65a3e77632966dd66486cb
proman <proman@gmail.com>
2025-03-03 23:08:20 +0530

    adding main.tf code

--- var_dataset_blob.tf
+++ var_dataset_blob.tf
@@ -1,41 +1,49 @@
 variable "data_factory_name" {
   description = "The name of the Data Factory."
   type        = string
+  default = "adf"
 }
 
 variable "resource_group_name" {
   description = "The name of the resource group containing the Data Factory."
   type        = string
+  default     = "rg"
 }
 
 variable "name" {
   description = "The name of the Data Factory dataset."
   type        = string
+  default     = "name"
 }
 
 variable "linked_service_name" {
   description = "The name of the linked service to use for the dataset."
   type        = string
+  default     = "linked_service"
 }
 
 variable "folder_path" {
   description = "The folder path in the Azure Blob Storage."
   type        = string
+  default     = "folder"
 }
 
 variable "file_name" {
   description = "The file name in the Azure Blob Storage."
   type        = string
+  default     = "filename"
 }
 
 variable "file_path" {
   description = "The file path in the Azure Blob Storage."
   type        = string
+  default     = "path"
 }
 
 variable "connection_string" {
   description = "The connection string for the Azure Blob Storage."
   type        = string
+  default     = "connection_string"
 }
 
 variable "data_factory_id" {
