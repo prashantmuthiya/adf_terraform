7f36d85fd225e20c1dbcd6d441bcb7c9091911b9
proman <proman@gmail.com>
2025-03-03 23:17:01 +0530

    adding main.tf code

--- dataset_blob.tf
+++ dataset_blob.tf
@@ -1,3 +1,17 @@
+resource "azurerm_storage_account" "this" {
+  name                     = var.storage_account_name
+  resource_group_name      = var.resource_group_name
+  location                 = "East US"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "this" {
+  name                  = var.container_name
+  storage_account_name  = azurerm_storage_account.this.name
+  container_access_type = "private"
+}
+
 resource "azurerm_data_factory_linked_service_azure_blob_storage" "this" {
   name                = var.linked_service_name
   connection_string   = var.connection_string
@@ -9,3 +23,4 @@ resource "azurerm_data_factory_dataset_azure_blob" "this" {
   linked_service_name = azurerm_data_factory_linked_service_azure_blob_storage.this.name
   data_factory_id     = var.data_factory_id
 }
+
--- var_dataset_blob.tf
+++ var_dataset_blob.tf
@@ -1,54 +1,41 @@
 variable "data_factory_name" {
   description = "The name of the Data Factory."
   type        = string
-  default = "adf"
+  default     = " adf"
 }
 
 variable "resource_group_name" {
   description = "The name of the resource group containing the Data Factory."
   type        = string
-  default     = "rg"
+  default     = " rg"
 }
 
 variable "name" {
   description = "The name of the Data Factory dataset."
   type        = string
-  default     = "name"
+  default     = " dataset"
 }
 
 variable "linked_service_name" {
   description = "The name of the linked service to use for the dataset."
   type        = string
-  default     = "linked_service"
-}
-
-variable "folder_path" {
-  description = "The folder path in the Azure Blob Storage."
-  type        = string
-  default     = "folder"
-}
-
-variable "file_name" {
-  description = "The file name in the Azure Blob Storage."
-  type        = string
-  default     = "filename"
-}
-
-variable "file_path" {
-  description = "The file path in the Azure Blob Storage."
-  type        = string
-  default     = "path"
+  default     = " linked-service"
 }
 
 variable "connection_string" {
   description = "The connection string for the Azure Blob Storage."
   type        = string
-  default     = "connection_string"
+  default     = "DefaultEndpointsProtocol"
 }
 
-variable "data_factory_id" {
-    type = string
-    description = "data factory id"
-    default = "adf_id"
-  
+variable "storage_account_name" {
+  description = "The name of the Azure Storage Account."
+  type        = string
+  default     = "examplestorageaccount"
+}
+
+variable "container_name" {
+  description = "The name of the Blob Storage container."
+  type        = string
+  default     = " container"
 }
