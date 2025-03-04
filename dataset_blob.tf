922f64ad031179d4118b58289807f63f9aeba4d8
proman <proman@gmail.com>
2025-03-04 19:57:04 +0530

    adding main.tf code

--- dataset_blob.tf
+++ dataset_blob.tf
@@ -1,5 +1,5 @@
 module "storage" {
-  source                      = "/modules/storage"
+  source                      = "modules/storage"
   #resource_group_name          = var.resource_group_name
   location                     = var.location
   account_tier                 = var.account
