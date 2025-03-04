9500d440a22c2287c2b8ef1972075f55e0e54154
proman <proman@gmail.com>
2025-03-04 15:57:34 +0530

    adding main.tf code

--- mysql_linked_service.tf
+++ mysql_linked_service.tf
@@ -4,14 +4,6 @@ resource "azurerm_data_factory_linked_service_mysql" "this" {
  # data_factory_name   = var.data_factory_name
   data_factory_id     = var.data_factory_id
   description         = var.description
- # integration_runtime = var.integration_runtime
+  integration_runtime = var.integration_runtime
   connection_string   = var.connection_string
-
-  dynamic "additional_properties" {
-    for_each = var.connection_properties.additional_properties 
-    content {
-      name  = additional_properties.key
-      value = additional_properties.value
-    }
-  }
 }
