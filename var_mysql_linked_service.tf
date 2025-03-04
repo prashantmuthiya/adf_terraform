fd87d68b1dd0687507d5b508138aa66780de8bee
proman <proman@gmail.com>
2025-03-04 15:56:03 +0530

    adding main.tf code

--- var_mysql_linked_service.tf
+++ var_mysql_linked_service.tf
@@ -1,21 +1,19 @@
 variable "resource_group_name" {
   description = "The name of the resource group."
   type        = string
-}
-
-variable "data_factory_name" {
-  description = "The name of the Data Factory."
-  type        = string
+  default     = "rg"
 }
 
 variable "data_factory_id" {
   description = "The ID of the Data Factory."
   type        = string
+  default     = "id"
 }
 
 variable "linked_service_name" {
   description = "The name of the Linked Service."
   type        = string
+  default     = "name"
 }
 
 variable "description" {
@@ -27,9 +25,11 @@ variable "description" {
 variable "integration_runtime" {
   description = "The integration runtime to be used by the Linked Service."
   type        = string
+  default     = "ir"
 }
 
 variable "connection_string" {
   description = "The connection string for the MySQL Linked Service."
   type        = string
+  default     = "connection_string"
 }
