f66ab298d5911f2ab8aa89cefa0aaa47c9a20e40
proman <proman@gmail.com>
2025-03-06 21:41:24 +0530

    adding main.tf code

--- azurerm_data_factory_linked_service_cosmosdb_var.tf
+++ azurerm_data_factory_linked_service_cosmosdb_var.tf
@@ -1,31 +1,37 @@
 variable "name" {
   description = "The name of the linked service."
   type        = string
+  default     = ""
 }
 
 variable "data_factory_id" {
   description = "The ID of the Data Factory in which to create the linked service."
   type        = string
+  default     = ""
 }
 
 variable "connection_string" {
   description = "The connection string for the Cosmos DB account."
   type        = string
+  default     = ""
 }
 
 variable "account_endpoint" {
   description = "The endpoint of the Cosmos DB account."
   type        = string
+  default     = ""
 }
 
 variable "account_key" {
   description = "The key of the Cosmos DB account."
   type        = string
+  default     = ""
 }
 
 variable "database" {
   description = "The database name within the Cosmos DB account."
   type        = string
+  default     = ""
 }
 
 variable "description" {
