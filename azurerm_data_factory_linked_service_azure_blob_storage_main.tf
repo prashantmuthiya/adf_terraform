a5aad6b89c416aa04f499ceb77ba95a1af5d6445
proman <proman@gmail.com>
2025-03-06 11:47:29 +0530

    adding main.tf code

---
+++ azurerm_data_factory_linked_service_azure_blob_storage_main.tf
@@ -0,0 +1,14 @@
+resource "azurerm_data_factory_linked_service_azure_blob_storage" "linked_service_azure_blob_storage" {
+  name            = var.linked_service_name
+  data_factory_id = var.data_factory_id
+  description     = var.description
+
+  key_vault_sas_token { 
+    linked_service_name = var.linked_service_name  # Ensure this is the correct linked service name for your Key Vault
+    secret_name         = var.secret
+  }
+
+  additional_properties = {
+    "sasUri" = var.sas_uri
+  }
+}
