0a5effa0d60aa422c92c5db18169a10693063be8
proman <proman@gmail.com>
2025-03-06 13:19:21 +0530

    adding main.tf code

---
+++ azurerm_data_factory_linked_service_snowflake_var.tf
@@ -0,0 +1,10 @@
+resource "azurerm_data_factory_linked_service_snowflake" "this" {
+  name            = var.name
+  data_factory_id = var.data_factory_id
+  connection_string = var.connection_string
+  
+  key_vault_password {
+    linked_service_name = var.key_vault_linked_service_name
+    secret_name         = var.snowflake_secret_name
+  }
+}
