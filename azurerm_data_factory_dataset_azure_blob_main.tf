5001540633e49ead22ee8408d6d12c04c00609e9
proman <proman@gmail.com>
2025-03-06 18:36:18 +0530

    adding main.tf code

--- azurerm_data_factory_dataset_azure_blob_main.tf
+++ azurerm_data_factory_dataset_azure_blob_main.tf
@@ -3,7 +3,12 @@ resource "azurerm_data_factory_dataset_azure_blob" "dataset_azure_blob" {
   data_factory_id     = var.data_factory_id
   linked_service_name = var.linked_service_name
 
-  path  = var.path
-  filename = var.filename
+  path      = var.path
+  filename  = var.filename
+
   parameters = var.parameters
+
+  additional_properties = var.additional_properties
+  description           = var.description
+  folder                = var.folder
 }
