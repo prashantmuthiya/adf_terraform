9b88a677f29f4e9448121a1d3f7396c56da78b77
proman <proman@gmail.com>
2025-03-07 21:21:51 +0530

    adding code

--- azurerm_data_factory_dataset_delimited_text_var.tf
+++ azurerm_data_factory_dataset_delimited_text_var.tf
@@ -1,16 +1,19 @@
 variable "dataset_name" {
   description = "The name of the dataset."
   type        = string
+  default = "value"
 }
 
 variable "data_factory_id" {
   description = "The ID of the Data Factory."
   type        = string
+  default = "value"
 }
 
 variable "linked_service_name" {
   description = "The name of the linked service."
   type        = string
+  default = "value"
 }
 
 variable "column_delimiter" {
