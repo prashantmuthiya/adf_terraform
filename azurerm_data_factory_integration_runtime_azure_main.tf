e66e3bbb31455efef08848f7af00792014612e88
proman <proman@gmail.com>
2025-03-09 13:14:05 +0530

    adding main.tf code

--- azurerm_data_factory_integration_runtime_azure_main.tf
+++ azurerm_data_factory_integration_runtime_azure_main.tf
@@ -1,11 +1,20 @@
-resource "azurerm_data_factory_integration_runtime_azure" "example" {
-  name                = var.integration_runtime_name
-  data_factory_id     = var.data_factory_id
-  location            = var.location
-  description         = var.description
-  time_to_live_min    = var.time_to_live_min
-  node_size           = var.node_size
-  number_of_nodes     = var.number_of_nodes
-  max_parallel_executions_per_node = var.max_parallel_executions_per_node
+resource "azurerm_data_factory_integration_runtime_azure" "integration_runtime_azure" {
+  name                   = var.integration_runtime_name
+  data_factory_id        = var.data_factory_id
+  location               = var.location
+  description            = var.description
+  time_to_live_min       = var.time_to_live_min
   virtual_network_enabled = var.virtual_network_enabled
+  cleanup_enabled        = var.cleanup_enabled
+  compute_type           = var.compute_type
+  core_count             = var.core_count
+
+  dynamic "timeouts" {
+    for_each = var.timeouts != null ? [var.timeouts] : []
+    content {
+      create = timeouts.value.create
+      update = timeouts.value.update
+      delete = timeouts.value.delete
+    }
+  }
 }
--- azurerm_data_factory_integration_runtime_azure_mvar.tf
+++ azurerm_data_factory_integration_runtime_azure_mvar.tf
@@ -1,53 +1,91 @@
 variable "integration_runtime_name" {
   description = "The name of the Integration Runtime."
   type        = string
-  default     = ""
+  default     = null
 }
 
 variable "data_factory_id" {
   description = "The ID of the Data Factory."
   type        = string
-  default     = ""
+  default     = null
 }
 
 variable "location" {
   description = "The location of the Integration Runtime."
   type        = string
-  default     = ""
+  default     = null
 }
 
 variable "description" {
   description = "A description for the Integration Runtime."
   type        = string
-  default     = ""
+  default     = null
 }
 
 variable "time_to_live_min" {
-  description = "Time to live in minutes."
+  description = "Time (in minutes) to keep the Integration Runtime alive."
   type        = number
-  default     = 0
-}
-
-variable "node_size" {
-  description = "The node size of the Integration Runtime."
-  type        = string
-  default     = "Standard_D2_v3"
-}
-
-variable "number_of_nodes" {
-  description = "The number of nodes."
-  type        = number
-  default     = 1
-}
-
-variable "max_parallel_executions_per_node" {
-  description = "Max parallel executions per node."
-  type        = number
-  default     = 1
+  default     = null
 }
 
 variable "virtual_network_enabled" {
-  description = "Should the Integration Runtime be connected to a Virtual Network."
+  description = "Enable or disable virtual network integration."
   type        = bool
-  default     = false
+  default     = null
 }
+
+variable "cleanup_enabled" {
+  description = "Specifies whether cleanup is enabled for the Integration Runtime."
+  type        = bool
+  default     = null
+}
+
+variable "compute_type" {
+  description = "The compute type for the Integration Runtime, e.g., 'General', 'MemoryOptimized'."
+  type        = string
+  default     = null
+}
+
+variable "core_count" {
+  description = "The number of cores assigned to the Integration Runtime."
+  type        = number
+  default     = null
+}
+
+variable "timeouts" {
+  description = "Timeout settings for create, update, and delete operations."
+  type = object({
+    create = optional(string)
+    update = optional(string)
+    delete = optional(string)
+  })
+  default = null
+}
+
+/*
+integration_runtime_name   = "example-ir"
+data_factory_id            = "/subscriptions/xxxx/resourceGroups/rg-name/providers/Microsoft.DataFactory/factories/adf-name"
+location                   = "East US"
+description                = "Example Integration Runtime"
+time_to_live_min           = 10
+virtual_network_enabled    = true
+cleanup_enabled            = true
+compute_type               = "General"
+core_count                 = 8
+timeouts = {
+  create = "30m"
+  update = "30m"
+  delete = "30m"
+}
+custom_properties = [
+  {
+    name  = "property1"
+    value = "value1"
+  },
+  {
+    name  = "property2"
+    value = "value2"
+  }
+]
+*/
+
