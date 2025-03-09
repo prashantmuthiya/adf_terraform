3f92be319485b1050a95cfe07b8ff4a515822d52
proman <proman@gmail.com>
2025-03-09 12:13:33 +0530

    adding main.tf code

---
+++ azurerm_data_factory_dataset_azure_blob_var.tf
@@ -0,0 +1,100 @@
+variable "dataset_name" {
+  description = "The name of the Azure Blob dataset"
+  type        = string
+}
+
+variable "data_factory_id" {
+  description = "The ID of the Data Factory"
+  type        = string
+}
+
+variable "linked_service_name" {
+  description = "The name of the linked service"
+  type        = string
+}
+
+variable "path" {
+  description = "The path to the dataset"
+  type        = string
+}
+
+variable "filename" {
+  description = "The filename in the dataset"
+  type        = string
+}
+
+variable "parameters" {
+  description = "A map of parameters for the dataset"
+  type        = map(string)
+  default     = null
+}
+
+variable "description" {
+  description = "A description of the dataset"
+  type        = string
+  default     = null
+}
+
+variable "folder" {
+  description = "The folder for the dataset"
+  type        = string
+  default     = null
+}
+
+variable "dynamic_path_enabled" {
+  description = "Enable dynamic path"
+  type        = bool
+  default     = null
+}
+
+variable "dynamic_filename_enabled" {
+  description = "Enable dynamic filename"
+  type        = bool
+  default     = null
+}
+
+variable "schema_columns" {
+  description = "Optional schema columns for the dataset"
+  type        = list(object({
+    name = string
+    type = string
+  }))
+  default     = null
+}
+
+
+/*module "adf_azure_blob_dataset" {
+  source = "./AzureDataFactory_AZ_dataset_blob"
+
+  dataset_name          = "example-dataset"
+  data_factory_id       = "your-data-factory-id"
+  linked_service_name   = "your-linked-service-name"
+  
+  path                  = "input/files/"
+  filename              = "example-file.csv"
+
+  parameters = {
+    param1 = "value1"
+    param2 = "value2"
+  }
+
+  description           = "Example Azure Blob dataset"
+  folder                = "example-folder"
+
+  dynamic_path_enabled  = true
+  dynamic_filename_enabled = false
+
+  schema_columns = [
+    {
+      name = "column1"
+      type = "String"
+    },
+    {
+      name = "column2"
+      type = "Int32"
+    }
+  ]
+}
+*/
+
+
