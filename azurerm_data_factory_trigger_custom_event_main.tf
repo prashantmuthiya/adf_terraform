5e7e1f4a4ae4dce0096ab4925447de84f363cb4d
proman <proman@gmail.com>
2025-03-10 17:39:54 +0530

    adding main.tf code

---
+++ azurerm_data_factory_trigger_custom_event_main.tf
@@ -0,0 +1,26 @@
+resource "azurerm_data_factory_trigger_custom_event" "trigger_custom_event" {
+  name                = var.trigger_name
+  data_factory_id     = var.data_factory_id
+  eventgrid_topic_id  = var.eventgrid_topic_id
+  events              = var.events
+  subject_begins_with = var.subject_begins_with
+  subject_ends_with   = var.subject_ends_with
+  description         = var.description
+  activated           = var.activated
+  additional_properties = var.additional_properties
+
+  dynamic "pipeline" {
+    for_each = (var.pipelines_enabled == true ? [1] : [])
+    content {
+      name       = var.pipelines.name
+      parameters = var.pipelines.parameters
+    }
+  }
+
+  timeouts {
+    create = var.create_timeout
+    read   = var.read_timeout
+    update = var.update_timeout
+    delete = var.delete_timeout
+  }
+}
---
+++ azurerm_data_factory_trigger_custom_event_var.tf
@@ -0,0 +1,114 @@
+
+variable "trigger_name" {
+  description = "The name of the custom event trigger."
+  type        = string
+}
+
+variable "data_factory_id" {
+  description = "The ID of the Data Factory."
+  type        = string
+}
+
+variable "eventgrid_topic_id" {
+  description = "The ID of the Event Grid topic."
+  type        = string
+}
+
+variable "events" {
+  description = "A list of events to trigger the custom event trigger."
+  type        = list(string)
+}
+
+variable "subject_begins_with" {
+  description = "The prefix filter for event subject."
+  type        = string
+}
+
+variable "subject_ends_with" {
+  description = "The suffix filter for event subject."
+  type        = string
+}
+
+variable "description" {
+  description = "A description of the custom event trigger."
+  type        = string
+}
+
+variable "activated" {
+  description = "Whether the trigger is activated."
+  type        = bool
+}
+
+variable "additional_properties" {
+  description = "Additional properties for the custom event trigger."
+  type        = map(string)
+}
+
+variable "pipelines_enabled" {
+  description = "Flag to enable or disable pipeline execution."
+  type        = bool
+  default     = false
+}
+
+variable "pipelines" {
+  description = "A list of pipelines to be executed."
+  type = list(object({
+    name       = string
+    parameters = map(string)
+  }))
+}
+
+variable "create_timeout" {
+  description = "Timeout for creating the custom event trigger."
+  type        = string
+  default     = "30m"
+}
+
+variable "read_timeout" {
+  description = "Timeout for reading the custom event trigger."
+  type        = string
+  default     = "30m"
+}
+
+variable "update_timeout" {
+  description = "Timeout for updating the custom event trigger."
+  type        = string
+  default     = "30m"
+}
+
+variable "delete_timeout" {
+  description = "Timeout for deleting the custom event trigger."
+  type        = string
+  default     = "30m"
+}
+
+output "trigger_id" {
+  value = azurerm_data_factory_trigger_custom_event.trigger_custom_event.id
+}
+
+/*
+// Example module usage
+module "adf_custom_event_trigger" {
+  source                = "./modules/adf_custom_event_trigger"
+  trigger_name          = "example-trigger"
+  data_factory_id       = "example-data-factory-id"
+  eventgrid_topic_id    = "example-eventgrid-topic-id"
+  events                = ["event1", "event2"]
+  subject_begins_with   = "example/subject/"
+  subject_ends_with     = "/suffix"
+  description           = "Example custom event trigger"
+  activated             = true
+  additional_properties = { key1 = "value1", key2 = "value2" }
+  pipelines_enabled     = true
+  pipelines = [
+    {
+      name       = "example-pipeline"
+      parameters = { param1 = "value1", param2 = "value2" }
+    }
+  ]
+  create_timeout = "40m"
+  read_timeout   = "40m"
+  update_timeout = "40m"
+  delete_timeout = "40m"
+}
+*/
